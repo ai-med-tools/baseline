6410b17414ddda701ac4c8d905031291fe852c80
Сергей Феодориди <s.feodoridi@webpractik.ru>
2023-10-18 15:29:55 +0300

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 python baseline.py core
 
-while $(kill -0 $(ps -C "python core" -o pid=))
+while $(kill -0 $(ps xawf -eo pid,command | grep "python core.py" | grep -v "grep" | cut -f6 -d " "))
 do
   sleep 1
 done
