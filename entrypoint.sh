142e603b3aa1af672523e7fa37528396aeebfe21
Сергей Феодориди <s.feodoridi@webpractik.ru>
2023-10-20 11:32:15 +0300

    Update fix entrypoint for windows

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 python baseline.py core
 
-while $(kill -0 $(ps xawf -eo pid,command | grep "python core.py" | grep -v "grep" | cut -f6 -d " "))
+while $(kill -0 $(ps xawf -eo pid,command | grep "python core.py" | grep -v "grep" | awk '{print $1}'))
 do
   sleep 1
 done
